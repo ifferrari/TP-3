4d80d22fa13d6e885e3721cc503b79257211b034
ifferrari <ignaciofferrari@gmail.com>
2015-04-30 11:59:45 -0300

    Update tp3.c
    Correcion del uso del "getCelsius" y del "for".

--- tp3.c
+++ tp3.c
@@ -1,9 +1,11 @@
-/* TP #1 "Tabla Fahrenheit-Celsius"
+/* TP #1 "Tabla Fahrenheit-Celsius usando funciones, GetCelsius, For y const"
 *Ignacio Francisco Ferrari
 *28/04/15
 */
 #include <stdio.h>
- 
+
+
+float getCelsius(float fahr);
 int main(void){
  
 int fahr;
@@ -13,8 +15,13 @@ int fahr;
  const int salto = 20;
  
 
-for (fahr = inferior; fahr <= mayor; fahr = fahr + salto)
-printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32)); 
+   for (fahr = inferior; fahr <= mayor; fahr = fahr + salto)
+     printf("%3d %6.1f\n", fahr, getCelsius(fahr)); 
 
-            }
+}
+
+float getCelsius (float fahr){
+      
+	return (5.0/9.0) * (fahr-32.0);
+}
 
